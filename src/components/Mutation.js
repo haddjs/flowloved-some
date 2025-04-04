ece873e3c7cc1324e0c6c29d33dc8999b3b2351d
kittendust21 <nadirhexonyte@gmail.com>
2025-04-05 02:05:12 +0700

    Project Done!

--- src/components/Mutation.js
+++ src/components/Mutation.js
@@ -160,6 +160,11 @@ const Mutation = () => {
                     </DialogHeader>
                     <DialogFooter className="gap-2 sm:gap-0">
                       <DialogClose asChild>
+                        <button className="border border-gray-300 hover:bg-gray-100 px-4 py-2 rounded-md">
+                          Cancel
+                        </button>
+                      </DialogClose>
+                      <DialogClose asChild>
                         <button
                           onClick={() =>
                             tx.type === "income"
@@ -169,11 +174,6 @@ const Mutation = () => {
                           className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md"
                         >
                           Delete
-                        </button>
-                      </DialogClose>
-                      <DialogClose asChild>
-                        <button className="border border-gray-300 hover:bg-gray-100 px-4 py-2 rounded-md">
-                          Cancel
                         </button>
                       </DialogClose>
                     </DialogFooter>
